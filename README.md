ba47a2888ad0ae9c8c1bade2a1bd8653fbf9908c
Vanessa Sochat <vsochat@stanford.edu>
2018-05-13 11:39:37 -0400

    adding basic Dockerfile, etc

---
+++ README.md
@@ -0,0 +1,14 @@
+# Keras-R
+
+This is a quick image to get you up and running with the R wrapper for Keras, the
+neural network API library. 
+
+ - [rstudio.keras.com](https://keras.rstudio.com/) has a brief introduction and tutorial.
+ - [Docker Hub](https://hub.docker.com/r/vanessa/keras-r/)
+
+You can use the image via Singularity as well:
+
+
+```bash
+singularity pull docker://vanessa/keras-r
+```
---
+++ install.R
@@ -0,0 +1,19 @@
+#!/usr/bin/env Rscript
+
+install.packages('devtools')
+devtools::install_github('rstudio/keras')
+install.packages('reticulate')
+reticulate::use_python('/opt/conda/bin/python')
+
+require(tensorflow)
+require(reticulate)
+require(keras)
+
+is_keras_available()
+system('which_python')
+sys.setenv(TENSORFLOW_PYTHON='/opt/conda/bin/python')
+use_python('/opt/conda/bin/python')
+
+py_discover_config('tensorflow')
+py_discover_config('keras')
+is_keras_available()
